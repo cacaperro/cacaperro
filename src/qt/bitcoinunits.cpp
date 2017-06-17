17350ce9f1e08bae0a84cb40538c658ae37928fa
quentin <quentin@debian.station>
2017-06-17 23:44:04 +0200

    fix

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -34,9 +34,9 @@ QString BitcoinUnits::name(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("GEO");
-    case mBTC: return QString("mGEO");
-    case uBTC: return QString::fromUtf8("μGEO");
+    case BTC: return QString("CACA");
+    case mBTC: return QString("mCACA");
+    case uBTC: return QString::fromUtf8("μCACA");
     default: return QString("???");
     }
 }
